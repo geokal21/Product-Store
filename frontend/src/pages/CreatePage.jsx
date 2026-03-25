40f24d976672b7e321d436fd2f0d82d2d0a23522
geokal21 <157402003+geokal21@users.noreply.github.com>
2026-03-26 00:15:05 +0200

    Add product creation logic to CreatePage
    Integrate product creation functionality using product store.

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,5 +1,6 @@
 import { Box, Button, Container, Heading, Input, useColorModeValue, VStack } from "@chakra-ui/react";
 import { useState } from "react";
+import { useProductStore } from "../store/product";
 
 const CreatePage = () => {
   const [newProduct, setNewProduct] = useState({
@@ -8,8 +9,10 @@ const CreatePage = () => {
     image: "",
   });
 
-  const handleAddProduct = () => {
-  console.log(newProduct);
+  const {createProduct} = useProduct()	
+
+  const handleAddProduct = async() => {
+    const {success, message} = await createProduct(newProduct)
   };
 	
   return (
