4aba299f86eb6275043d27c2b7669a322a83da99
geokal21 <157402003+geokal21@users.noreply.github.com>
2026-03-25 14:43:35 +0200

    Implement product creation form in CreatePage
    Added a form to create a new product with input fields.

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,9 +1,34 @@
-const CreatePage = () => {
-  return (
-    <div>
-      CreatePage
-    </div>
-  )
-}
+import { Box, Container, Heading, useColorModeValue, VStack } from "@chakra-ui/react";
+import { useState } from "react";
 
-export default CreatePage
+const CreatePage = () => {
+  const [newProduct, setNewProduct] = useState({
+    name: "",
+    price: "",
+    image: "",
+  });
+  return <Container maxW={"container.sm"}>
+    <VStack 
+      spacing={8}
+    >
+      <Heading as={"h1"} size={"2xl"} textAlign={"center"} mt={8}>
+        Create New Product
+      </Heading>
+      <Box
+        w={"full"} bg={useColorModeValue("white", "gray.800")}
+        p={6} rounded={lg} shadow={md}
+      >
+        <VStack spacing={4}>
+        <Input 
+          placeholder="Product Name"
+          name="name"
+          value={newProduct.name}
+          onchange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}
+        />
+        </VStack>
+      </Box>
+    </VStack>
+  </Container>;
+};
+
+export default CreatePage;
