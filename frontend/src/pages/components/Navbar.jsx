4ed230114ebff3a763b9debb09110b76ada86799
geokal21 <157402003+geokal21@users.noreply.github.com>
2026-03-25 20:51:12 +0200

    Enhance Navbar with product store link and theme toggle

--- frontend/src/pages/components/Navbar.jsx
+++ frontend/src/pages/components/Navbar.jsx
@@ -1,7 +1,16 @@
-import { Container } from "@chakra-ui/react";
+import { Button, Container, Flex, HStack, Text, useColorMode } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+import { PlusSquareIcon } from "@chakra-ui/icons";
+import { IoMoon } from "react-icons/io5";
+import { LuSun } from "react-icons/lu";
 
 const Navbar = () => {
-  return <Container maxW={"1140px"} px={4}>
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { products } = useProductStore();
+
+  return (
+     <Container maxW={"1140px"} px={4}>
     <Flex
       h={16}
       alignItems={"center"}
@@ -11,9 +20,33 @@ const Navbar = () => {
         sm: "row" 
       }}
     >
+
+      <Text
+        fontSize={{ base: "22", sm: "28" }}
+					fontWeight={"bold"}
+					textTransform={"uppercase"}
+					textAlign={"center"}
+					bgGradient={"linear(to-r, cyan.400, blue.500)"}
+					bgClip={"text"}
+				>
+					<Link to={"/"}>Product Store 🛒</Link>
+      </Text>
+
+      <HStack spacing={2} alignItems={"center"}>
+          <Link to={"/create"}>
+          <Button>
+            <PlusSquareIcon fontSize={20} />
+          </Button>
+          </Link>
+          <Button onClick={toggleColorMode}>
+            {colorMode === "light" ? <IoMoon /> : <LuSun size="20" />}
+          </Button>
+
+      </HStack>
         
     </Flex>
-  </Container>;
+   </Container> 
+  );
 };
 
 export default Navbar;
