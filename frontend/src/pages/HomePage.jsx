44bb96313a398c0accd9b8ae76ed3d78c07a0813
geokal21 <157402003+geokal21@users.noreply.github.com>
2026-03-29 21:15:50 +0300

    Implement product fetching in HomePage component
    Added product fetching functionality to HomePage.

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,6 +2,13 @@ import { Container, SimpleGrid, Text, VStack } from '@chakra-ui/react';
 import { Link } from "react-router-dom";
 
 const HomePage = () => {
+	const { fetchProducts, products } = useProductStore();
+
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
+  console.log("products", products);
+	
   return (
     <Container maxW='container.xl' py={12}>
       <VStack spacing={8}>
