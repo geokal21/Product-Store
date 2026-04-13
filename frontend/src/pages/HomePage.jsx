399b5b3c187b91a89a6414cb04e0c806398e8a53
geokal21 <157402003+geokal21@users.noreply.github.com>
2026-04-13 19:27:08 +0300

    Implement conditional rendering for no products
    Added conditional rendering for no products message.

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -40,7 +40,8 @@ const HomePage = () => {
            ))}	  
        </SimpleGrid>
 
-        <Text fontSize='xl' textAlign={"center"} fontWeight='bold' color='gray.500'>
+        {products.length === 0 && (
+          <Text fontSize='xl' textAlign={"center"} fontWeight='bold' color='gray.500'>
           No products found 😢{" "}
           <Link to={"/create"}>
               <Text as='span' color='blue.500' _hover={{ textDecoration: "underline" }}>
@@ -48,6 +49,7 @@ const HomePage = () => {
               </Text>
           </Link>
         </Text>
+        )}
       </VStack>
     </Container>
   )
