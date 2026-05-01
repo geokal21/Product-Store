b07364bb254eb615bda52838bfc51dda203ec8ae
geokal21 <157402003+geokal21@users.noreply.github.com>
2026-05-01 13:28:51 +0300

    Improve product update and delete handling
    Refactor product update logic to handle success and error messages.

--- frontend/src/pages/components/ProductCard.jsx
+++ frontend/src/pages/components/ProductCard.jsx
@@ -36,8 +36,25 @@ const ProductCard = ({product}) => {
   };
 
   const handleUpdateProduct = async (pid, updatedProduct) => {
-	await updateProduct(pid, updatedProduct);
+	const { success, message } = await updateProduct(pid, updatedProduct);
     onClose();
+	if (!success) {
+      toast({
+        title: "Error",
+        description: message,
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+    } else {
+      toast({
+        title: "Success",
+        description: message,
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+      });
+    }  
   };
 	
   return (
